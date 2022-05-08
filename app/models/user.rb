46b08ed46acff099cc92881810f6afe7b7cca949
Ashley Lampard <ashleylampard@live.com>
2022-05-08 06:23:05 +0100

    created purchases model and controller and added routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :plants, dependent: :destroy
+  has_many :purchases, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
