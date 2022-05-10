ec1c4b06135b7d93ff15204b8abac01dc484af7d
Ashley Lampard <ashleylampard@live.com>
2022-05-10 06:18:56 +0100

    js purchase controls functionality and side image preview bar added

--- app/javascript/controllers/purchase_controls_controller.js
+++ app/javascript/controllers/purchase_controls_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "counter" ]
+  static targets = [ "counter", "price" ]
 
-  increment() {
-    this.counterTarget.innerText = Number(this.counterTarget.innerText) + 1
-    this.dataset.count = Number(this.dataset.count) + 1;
+  increment(event) {
+    let offsetValue = Number(event.path[0].dataset.offset)
+    if ( (Number(this.counterTarget.innerText) > 1 && offsetValue == -1) || (Number(this.counterTarget.innerText) < 10 && offsetValue == 1) ) {
+      this.counterTarget.innerText = Number(this.counterTarget.innerText) + offsetValue
+
+    }
   }
 }
