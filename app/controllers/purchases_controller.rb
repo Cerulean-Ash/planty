d6509c200a19141c37c54eac8bc95a00f6c6a811
Ashley Lampard <ashleylampard@live.com>
2022-05-12 20:39:42 +0100

    added ability to add multiple items to cart

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,11 +4,13 @@ class PurchasesController < ApplicationController
   before_action :set_purchase, only: [:show, :edit, :update, :destroy]
 
   def create
-    @purchase = Purchase.new
-    @purchase.user = current_user
+    # @purchase = Purchase.new
+    # @purchase.quantity_purchased = 1
     plant = Plant.find(params[:plant_id])
-    @purchase.plant = plant
-    @purchase.cart = @cart
+    # @purchase.plant = plant
+    # @purchase.cart = @cart
+    @purchase = @cart.add_product(plant)
+    @purchase.user = current_user
     if @purchase.save
       redirect_to cart_path
     else
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,18 @@
 class Cart < ApplicationRecord
   has_many :purchases, dependent: :destroy
+
+  def add_product(plant)
+    current_item = Purchase.find_by(plant_id: plant.id, cart_id: id)
+    if current_item
+      if current_item.quantity_purchased == nil
+        current_item.quantity_purchased = 1
+      end
+      current_item.quantity_purchased += 1
+    else
+      current_item = Purchase.new(plant_id: plant.id, cart_id: id)
+      current_item.quantity_purchased = 1
+    end
+    current_item
+  end
+
 end
