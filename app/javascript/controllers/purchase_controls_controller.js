dbaa4335db300ef6ad7d5a4c5efbd7eb8b80a048
Ashley Lampard <ashleylampard@live.com>
2022-05-18 13:26:31 +0100

    can only add clippings to cart up to the number available

--- app/javascript/controllers/purchase_controls_controller.js
+++ app/javascript/controllers/purchase_controls_controller.js
@@ -1,18 +1,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "counter", "price", "price_per" ]
+  static targets = [ "counter", "price", "price_per", "num_available" ]
 
   #updateNumbers = (count) => {
-    const cost = this.price_perTarget.dataset.costper * count
-    this.priceTarget.value = `Pay ${cost.toFixed(2)}`
+    const cost = this.price_perTarget.dataset.costper * count;
+    this.priceTarget.value = `Pay ${cost.toFixed(2)}`;
+  }
+
+  #maxNumbers = () => {
+    const num = this.num_availableTarget.dataset.numavailable;
+    return num;
   }
 
   increment(event) {
-    let offsetValue = Number(event.path[0].dataset.offset)
-    if ( (Number(this.counterTarget.value) > 1 && offsetValue == -1) || (Number(this.counterTarget.value) < 10 && offsetValue == 1) ) {
-      this.counterTarget.value = Number(this.counterTarget.value) + offsetValue
-      this.#updateNumbers(this.counterTarget.value)
+    let offsetValue = Number(event.path[0].dataset.offset);
+    if ( (Number(this.counterTarget.value) > 1 && offsetValue == -1) || (Number(this.counterTarget.value) < this.#maxNumbers() && offsetValue == 1) ) {
+      this.counterTarget.value = Number(this.counterTarget.value) + offsetValue;
+      this.#updateNumbers(this.counterTarget.value);
+
     }
   }
 
