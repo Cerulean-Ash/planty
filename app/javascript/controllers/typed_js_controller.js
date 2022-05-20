f421ffe9d5e0a9e05f4a5067228cf9285badc502
Ashley Lampard <ashleylampard@live.com>
2022-05-20 06:57:42 +0100

    resolved question mark appearing before emoji loads

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,7 +5,8 @@ import Typed from "typed.js";
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ['🌱 🪴 🌵 🌿 ☘️ 🌲 🌳 🌴 🌱 🪴 🌵 🌿 🌱 🪴 🌵 🌿 ☘️ 🌲 🌳 🌴 🌱 🪴 🌵 🌿 ☘️ 🌲 🌳 🌴 '],
+      // unicode codes used https://unicode.org/emoji/charts/full-emoji-list.html
+      strings: ['&#x1f331; &#x1fab4; &#x1f341; &#x1f335; &#x1f33f; &#x1f340; &#x1f332; &#x1f333; &#x1f334; &#x1f343;'],
       typeSpeed: 100,
       loop: false,
       showCursor: false
